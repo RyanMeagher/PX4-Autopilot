8490167e687cf9665dd17a8dda91b6f7429753c7
Bruce Meagher <bmeagher@glpsoftware.com>
2021-05-24 13:02:02 -0700

    Measurememt fix.

--- src/drivers/barometer/ms5837/ms5837.cpp
+++ src/drivers/barometer/ms5837/ms5837.cpp
@@ -377,7 +377,7 @@ int MS5837::collect()
 		/* MS5837 temperature compensation */
 		if (TEMP < 2000) {
 
-			int32_t T2 = 3 * ((int64_t)POW2(dT) >> 33);
+			int32_t T2 = (3 * (int64_t)POW2(dT)) >> 33;
 
 			int64_t f = POW2((int64_t)TEMP - 2000);
 			int64_t OFF2 = 3 * f >> 1;
@@ -394,7 +394,7 @@ int MS5837::collect()
 			_OFF  -= OFF2;
 			_SENS -= SENS2;
 		} else {
-			int32_t T2 = 2 *((int64_t)POW2(dT) >> 37);
+			int32_t T2 = (2 *(int64_t)POW2(dT)) >> 37;
 
 			int64_t f = POW2((int64_t)TEMP - 2000);
 			int64_t OFF2 = 1 * f >> 4;
